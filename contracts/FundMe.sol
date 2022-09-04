d3f20c542f0f1a88df32476e1b01018e3c2373f5
unknown <robotxelliot@gmail.com>
2022-09-04 16:52:47 +0700

    initial commit

---
+++ contracts/FundMe.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
+error FundMe__NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+    uint256 public constant MINIMUM_USD = 50 * 10**18;
+    address private immutable i_ower;
+    address[] public s_funders;
+    mapping(address => uint256) s_addressToAmountFunded;
+    AggregatorV3Interface private s_priceFeed;
+
+    modifier onlyOwner() {
+        if (msg.sender != i_ower) revert FundMe__NotOwner();
+        _;
+    }
+
+    constructor(address priceFeed) {
+        s_priceFeed = AggregatorV3Interface(priceFeed);
+        i_ower = msg.sender;
+    }
+
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
+            "You need to spend more ETH!"
+        );
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+    }
+
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < s_funders.length;
+            funderIndex++
+        ) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        s_funders = new address[](0);
+        (bool success, ) = i_ower.call{value: address(this).balance}("");
+        require(success);
+    }
+
+    function cheaperWithdraw() public onlyOwner {
+        address[] memory funders = s_funders;
+        // mappings can't be in memory, sorry!
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        s_funders = new address[](0);
+        // payable(msg.sender).transfer(address(this).balance);
+        (bool success, ) = i_ower.call{value: address(this).balance}("");
+        require(success);
+    }
+
+    function getAddressToAmountFunded(address fundingAddress)
+        public
+        view
+        returns (uint256)
+    {
+        return s_addressToAmountFunded[fundingAddress];
+    }
+
+    function getVersion() public view returns (uint256) {
+        return s_priceFeed.version();
+    }
+
+    function getFunder(uint256 index) public view returns (address) {
+        return s_funders[index];
+    }
+
+    function getOwner() public view returns (address) {
+        return i_ower;
+    }
+
+    function getPriceFeed() public view returns (AggregatorV3Interface) {
+        return s_priceFeed;
+    }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,42 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-deploy")
+require("dotenv").config()
+require("@nomiclabs/hardhat-ethers")
+/** @type import('hardhat/config').HardhatUserConfig */
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+            blockConfirmations: 6,
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.8",
+            },
+            {
+                version: "0.6.6",
+            },
+        ],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+    },
+}
